7d7dc3a2b82363e8ed1b8edd82affda53a6da5e2
moonlight <moonlight@nuaa.edu.cn>
2025-06-07 17:30:38 +0000

    添加Pathtools Python3.12兼容版本

---
+++ package/pathtools/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# setup.py: Setup information.
+# Copyright (C) 2010 Yesudeep Mangalapilly <yesudeep@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import os
+import importlib.util
+import importlib.machinery
+from distutils.core import setup
+
+PKG_DIR = 'pathtools'
+def load_source(modname, filename):
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    # The module is always executed and not cached in sys.modules.
+    # Uncomment the following line to cache the module.
+    # sys.modules[module.__name__] = module
+    loader.exec_module(module)
+    return module
+version = load_source('pathtools.version', os.path.join(PKG_DIR, 'version.py'))
+
+def read_file(filename):
+    """
+    Reads the contents of a given file relative to the directory
+    containing this file and returns it.
+
+    :param filename:
+        The file to open and read contents from.
+    """
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(name='pathtools',
+      version=version.VERSION_STRING,
+      description='File system general utilities',
+      long_description=read_file('README'),
+      author="Yesudeep Mangalapilly",
+      author_email="yesudeep@gmail.com",
+      license="MIT License",
+      url="http://github.com/gorakhargosh/pathtools",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: System :: Filesystems',
+          'Topic :: Utilities',
+          ],
+      packages=['pathtools']
+      )
